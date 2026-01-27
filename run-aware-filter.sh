0080e539bcbb941d408d823842ed58a27198cb5d
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2026-01-27 14:40:28 +0200

    Full paths

--- run-aware-filter.sh
+++ run-aware-filter.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 cd /home/rantahj1/aware_filter
-source venv/bin/activate
-exec python -m aware_filter
+source /home/rantahj1/aware_filter/venv/bin/activate
+exec /home/rantahj1/aware_filter/venv/bin/python -m aware_filter
