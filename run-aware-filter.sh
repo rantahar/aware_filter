e81348843ab69cf0f06f8eafe74d7e22764daed1
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2026-01-27 14:45:15 +0200

    use gunicorn

--- run-aware-filter.sh
+++ run-aware-filter.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 cd /home/rantahj1/aware_filter
 source /home/rantahj1/aware_filter/venv/bin/activate
-exec /home/rantahj1/aware_filter/venv/bin/python -c "from aware_filter import main; main()"
+exec /home/rantahj1/aware_filter/venv/bin/gunicorn --config gunicorn.conf.py wsgi:app
