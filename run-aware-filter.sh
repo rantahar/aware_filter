42d689fa4f7dc098e0fa94d10db5160405569406
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2026-01-27 14:41:27 +0200

    run te package

--- run-aware-filter.sh
+++ run-aware-filter.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 cd /home/rantahj1/aware_filter
 source /home/rantahj1/aware_filter/venv/bin/activate
-exec /home/rantahj1/aware_filter/venv/bin/python -m aware_filter
+exec /home/rantahj1/aware_filter/venv/bin/python -c "from aware_filter import main; main()"
